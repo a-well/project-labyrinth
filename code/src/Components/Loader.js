b781d7cc4013284274671647e01160dcc9d7e399
a-well <amandaemkwellington@gmail.com>
2023-03-08 11:05:57 +0100

    Cleanup

--- code/src/Components/Loader.js
+++ code/src/Components/Loader.js
@@ -4,11 +4,11 @@ import loaderAnimation from 'Lotties/Loader'
 import Lottie from 'lottie-react'
 
 const LoaderContainer = styled.div`
-display: grid;
-align-content: center;
-width: 200px;
-margin: 0 auto;
-height: 100vh;
+  display: grid;
+  align-content: center;
+  width: 200px;
+  margin: 0 auto;
+  height: 100vh;
 `
 
 const Loader = () => (
